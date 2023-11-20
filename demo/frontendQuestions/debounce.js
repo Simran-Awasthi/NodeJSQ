c050eac35265108564487e27dbc2d8b244f93d68
Simran-Awasthi <simranawasthi03@gmail.com>
2023-11-21 01:30:30 +0530

    debounceSeach

--- demo/frontendQuestions/debounce.js
+++ demo/frontendQuestions/debounce.js
@@ -1,9 +1,20 @@
-function debounceInvocation(wait, func) {
+// function debounceInvocation(wait, func) {
+//   let timeId = 0;
+//   return function (...args) {
+//     clearTimeout(timeId);
+//     timeId = setTimeout(() => {
+//       func.apply(this, args);
+//     }, wait);
+//   };
+// }
+const debounceSeach = (wait, func) => {
   let timeId = 0;
   return function (...args) {
     clearTimeout(timeId);
     timeId = setTimeout(() => {
-      func.apply(this, args);
+      func(...args);
     }, wait);
   };
-}
+};
+const debouncedSearch = debounce(searchFunction, 300);
+ 
